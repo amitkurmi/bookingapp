b9b6052cc71e0effbb3e67d73172bed7ae391ae9
amitkurmi <amit.201704090@tulas.edu.in>
2023-04-21 17:42:55 +0545

    ....

---
+++ lib/screens/hotel_screen.dart
@@ -0,0 +1,54 @@
+import 'package:bookingapp/utils/app_layout.dart';
+import 'package:bookingapp/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HotelScreen extends StatelessWidget {
+  final Map<String, dynamic> hotel;
+  const HotelScreen({super.key, required this.hotel});
+
+  @override
+  Widget build(BuildContext context) {
+    print('Hotel price is ${hotel['price']}');
+    final size = AppLayout.getSize(context);
+    return Container(
+      width: size.width * 0.6,
+      height: 350,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
+      margin: const EdgeInsets.only(right: 17, top: 5),
+      decoration: BoxDecoration(
+          color: Styles.primaryColor,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.shade200, blurRadius: 2, spreadRadius: 1)
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+                color: Styles.primaryColor,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/${hotel['image']}"))),
+          ),
+          const Gap(10),
+          Text(
+            hotel['place'],
+            style: Styles.headLineStyle2.copyWith(color: Styles.kakiColor),
+          ),
+          Text(
+            hotel['destination'],
+            style: Styles.headLineStyle3.copyWith(color: Colors.white),
+          ),
+          Text(
+            '\$${hotel['price']}/night',
+            style: Styles.headLineStyle1.copyWith(color: Styles.kakiColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
